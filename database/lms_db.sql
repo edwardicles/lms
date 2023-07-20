82cd238b0194ce2b1cac28dae2276da4662298ed
edwardicles <qejbballacillo@tip.edu.ph>
2023-07-20 11:25:18 +0800

    Update lms_db.sql

--- database/lms_db.sql
+++ database/lms_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 20, 2023 at 05:11 AM
+-- Generation Time: Jul 20, 2023 at 05:24 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
